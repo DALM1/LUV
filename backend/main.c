c25a970f961df56d03ef6ba062857a0ac82ffbc8
Dimitri Almon <dimitri.almon@gmail.com>
2024-10-15 13:57:25 +0200

    Updates

--- backend/main.c
+++ backend/main.c
@@ -5,10 +5,10 @@
 
 #define PORT 8888
 
-MHD_Result answer_to_connection(void *cls, struct MHD_Connection *connection,
-                                const char *url, const char *method,
-                                const char *version, const char *upload_data,
-                                size_t *upload_data_size, void **con_cls) {
+enum MHD_Result answer_to_connection(void *cls, struct MHD_Connection *connection,
+                                     const char *url, const char *method,
+                                     const char *version, const char *upload_data,
+                                     size_t *upload_data_size, void **con_cls) {
     const char *page = "<html><body>WELCOME TO LUV</body></html>";
 
     struct MHD_Response *response;
